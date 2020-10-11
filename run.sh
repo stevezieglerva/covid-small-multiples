7d91fdd585487634cce809541d219c384a5a20f1
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-10-11 09:09:34 -0400

    change s3 upload location

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ curl -o all-states-history.csv https://covidtracking.com/data/download/all-state
 
 python3 create_small_multiples.py 
 
-aws s3 cp ./ s3://nerdthoughts.net/images/ --recursive --exclude "*" --include "state_*.png"
-aws s3 cp ./ s3://www.nerdthoughts.net/images/ --recursive --exclude "*" --include "state_*.png"
+aws s3 cp ./ s3://svz-public/covid_multiple_smalls/ --recursive --exclude "*" --include "covid_*.png"
+
 
